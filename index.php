d569c505a2f3ec0e33c3bb477041c4d6dc8fbeea
Ron Marsden <rm@maxwebsolutions.com>
2025-03-25 09:25:54 +0000

    Update content rendering logic in index.php
    Added conditional checks for front page, blog, and category views. Ensured appropriate templates and content are displayed based on the page type.

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
-<?php get_header() ?>
-<main>
-    <?php the_content() ?>
-</main>
-<?php get_footer() ?>
+<?php get_header(); ?>
+    <main>
+        <?php if(is_front_page()) {
+            the_content();
+        } elseif(is_home() || is_category()) {
+            get_template_part('archive');
+        } else {
+            the_content();
+        }?>
+    </main>
+<?php get_footer(); ?>
