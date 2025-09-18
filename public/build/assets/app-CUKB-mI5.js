9e11854711bd76fbc66201e70c70d81b671d63a4
Ron Marsden <rm@maxwebsolutions.com>
2025-09-18 17:31:42 +0100

    Add new lazy-loading and critical assets
    Introduce updated lazy-loading logic and critical CSS for improved performance. Added new scripts and styles for app and editor functionalities, alongside a manifest file to manage asset references.

---
+++ public/build/assets/app-CUKB-mI5.js
@@ -0,0 +1,1 @@
+import{l as d}from"./deferring-vsbnP93r.js";document.addEventListener("DOMContentLoaded",()=>{d()});
---
+++ public/build/assets/editor-K-bifxVc.js
@@ -0,0 +1,1 @@
+import{l as a}from"./deferring-vsbnP93r.js";window.addEventListener("load",()=>{a()});
