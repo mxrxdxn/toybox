00e7675e3dfd6e06b6803ab6eaf4aa608742994b
Ron Marsden <rm@maxwebsolutions.com>
2026-04-22 09:37:17 +0100

    Add Vite build outputs and manifest for asset bundling and lazy-loading

---
+++ public/build/js/resources-js-app-DezA9VHm.js
@@ -0,0 +1,2 @@
+import{lazyLoading as e}from"./resources-js-libs-deferring-CIUjTRux.js";document.addEventListener(`DOMContentLoaded`,()=>{e()});
+//# sourceMappingURL=resources-js-app-DezA9VHm.js.map
---
+++ public/build/js/resources-js-editor-CKjHY-tf.js
@@ -0,0 +1,2 @@
+import{lazyLoading as e}from"./resources-js-libs-deferring-CIUjTRux.js";window.addEventListener(`load`,()=>{e()});
+//# sourceMappingURL=resources-js-editor-CKjHY-tf.js.map
