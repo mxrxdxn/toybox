449600fc9ad5cb9a8eeb6db2dddd3bba1e530a50
Ron Marsden <rm@maxwebsolutions.com>
2026-04-22 09:26:48 +0100

    Refactor conditional layout logic in `index.php` for improved readability and formatting

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php get_header(); ?>
     <main>
-        <?php if(is_front_page()) {
+        <?php
+
+        if (is_front_page()) {
             the_content();
-        } elseif(is_home() || is_category()) {
+        } elseif (is_home() || is_category()) {
             get_template_part('archive');
         } else {
             the_content();
-        }?>
+        }
+
+        ?>
     </main>
 <?php get_footer(); ?>
